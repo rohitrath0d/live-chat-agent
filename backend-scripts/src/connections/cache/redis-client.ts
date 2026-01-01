48d8db835dc89e2147d3a678515b496f37306120
Rohit <rohit.rthd.04@gmail.com>
2026-01-01 14:14:55 +0530

    changes in rdis configs

--- backend-scripts/src/connections/cache/redis-client.ts
+++ backend-scripts/src/connections/cache/redis-client.ts
@@ -25,6 +25,7 @@ async function getClient(): Promise<RedisClientType> {
         host: REDIS_HOST,
         port: REDIS_PORT,
         tls: true,   // important for Redis Cloud - t- deploy on TLS port - for production use
+        rejectUnauthorized: false
       },
       username: REDIS_USERNAME,
       password: REDIS_PASSWORD,
