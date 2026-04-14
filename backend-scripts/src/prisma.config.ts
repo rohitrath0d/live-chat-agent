8508eee6219fcb86b2c48cc8979a25ca8af768ae
Rohit <rohit.rthd.04@gmail.com>
2026-04-14 18:05:22 +0530

    updates and added env examples

--- backend-scripts/src/prisma.config.ts
+++ backend-scripts/src/prisma.config.ts
@@ -4,9 +4,9 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "src/prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "src/prisma/migrations",
   },
   datasource: {
     // When exactOptionalPropertyTypes: true:
